f46fdacb73603cd97d9368abb73bd3d29b1b56d8
Masato Hori <rillomas@gmail.com>
2021-08-09 12:03:32 +0900

    reentering digital design

--- blink_cyclone_10.v
+++ blink_cyclone_10.v
@@ -2,20 +2,16 @@
 module blink_cyclone_10 (
     input wire clk, // 50MHz input clock
     output wire LED0, // LED ouput
-	 output wire LED1 // LED ouput
+    output wire LED1 // LED ouput
 );
 // create a binary counter
-reg [31:0] cnt0;
-reg [31:0] cnt1;
+reg [31:0] cnt;
 initial begin
-cnt0 <= 32'h00000000;
-cnt1 <= 32'h00000000;
+    cnt <= 32'h00000000;
 end
 always @(posedge clk) begin
-cnt0 <= cnt0 + 1;
-cnt1 <= cnt1 + 1;
+    cnt <= cnt + 1;
 end
 //assign LED to 25th bit of the counter to blink the LED at a few Hz
-assign LED0 = cnt0[24];
-assign LED1 = cnt0[22];
+assign LED0 = cnt[24];
 endmodule

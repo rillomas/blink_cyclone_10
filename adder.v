feb9b474ba5ed3de4fa3b5344abd4653489df3f3
Masato Hori <rillomas@gmail.com>
2021-08-18 21:15:02 +0900

    trying to blink LED through arduino interface

--- adder.v
+++ adder.v
@@ -5,8 +5,8 @@ module Adder (
 	output led_0, // LED ouput
 	output led_1 // LED ouput
 );
-	// wire sum;
-	FullAdder fa(.a(switch_0), .b(switch_1), .ci(switch_2), .s(led_0), .co(led_1));
+	wire sum;
+	FullAdder fa(.a(switch_0), .b(switch_1), .ci(switch_2), .s(sum), .co(led_1));
 	// We invert the bit since we're connecting to a anode common LED (0 will light the LED)
-	// assign led_0 = ~sum;
+	assign led_0 = ~sum;
 endmodule
